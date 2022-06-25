241074f2087dbb65f9fa6ca81a9f703ed6483d61
Dappa <94505216+ReyyNada@users.noreply.github.com>
2022-06-26 02:01:29 +0700

    .

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,5 +1,6 @@
 # (©)Codexbotz
 # Recide by @Mafia_Tobatz
+# Recode By @mahadappa
 # Kalo clone Gak usah hapus 
 # gue tandain akun tele nya ngentod
 
@@ -14,7 +15,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Link Bokep: <a href='www.xnxx.com'>Klik Disini</a></b>\n",
+            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Channel : @kokbisadek\n • Owner : @mahadappa< • Link Bokep: <a href='www.xnxx.com'>Klik Disini</a></b>\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("🔅 ᴛᴜᴛᴜᴘ 🔅", callback_data="close")]]
